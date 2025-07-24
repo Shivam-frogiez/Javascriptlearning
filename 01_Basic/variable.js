0b583c2438f3ff83a642d0afb9753bcee1c7c13d
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-07-24 05:26:45 +0000

    variable

---
+++ 01_Basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "Shiv@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "This@gmail.com"
+accountPassword = "akf;lk"
+accountCity = "jaipur"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
