1e3c5b8e91cbc827a571b68ce202df690639d46c
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-07-25 05:50:36 +0000

    Const, let & Var

---
+++ 02_Let/let.js
@@ -0,0 +1,12 @@
+let a = 5
+console.log(a)
+
+const alphaId = 456
+
+// alphaId = 54
+
+console.log(alphaId)
+
+// const values are not for  redeclaring it
+// for declaring vales we should use let
+// do not use var because it creates problem in block scope
