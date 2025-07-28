8308612cd0bf59388c432d39d703c188ffa5fe59
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-07-28 05:47:43 +0000

    string

---
+++ 04_String/string.js
@@ -0,0 +1,5 @@
+const name = "Shiv"
+const repoaccount = 50
+console.log(name + repoaccount + "Testing");
+// string etnerpulation
+console.log(`this is my ${name} and my repoaccount ${repoaccount}`);
