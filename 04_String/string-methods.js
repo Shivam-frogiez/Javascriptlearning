fc87e60bbe6f519348eb38088e6de5f0417845a6
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-07-30 05:22:40 +0000

    nums and data

---
+++ 04_String/string-methods.js
@@ -0,0 +1,3 @@
+const gamerName = "Thisisexample"
+console.log(gamerName[0]);
+console.log(gamerName);
---
+++ 05_Nums_n_math/nums.js
@@ -0,0 +1,10 @@
+const score = 400
+console.log(typeof(score), score);
+
+const balance = new Number(1000)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+
