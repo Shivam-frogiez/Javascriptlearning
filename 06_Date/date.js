5d91e29368c10aebb69ee5286b75dd9b5c975a6c
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-07-31 05:34:07 +0000

    date

---
+++ 06_Date/date.js
@@ -0,0 +1,30 @@
+// date in js
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+// let mycreateddate = new Date(2024, 1, 23)
+// let mycreateddate = new Date(2024, 2, 23, 5, 3, 59)
+let mycreateddate = new Date("01-14-2025")
+// console.log(mycreateddate.toDateString());
+
+let mytimestamp = Date.now()
+
+// console.log(mytimestamp);
+// console.log(mycreateddate.getTime());
+
+// console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay);
+
+
+
+
+
