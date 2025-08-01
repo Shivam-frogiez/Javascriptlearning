4b5e54436aa29f47b0d137688064475c32140246
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-01 06:02:58 +0000

    array

---
+++ 07_Array/array.js
@@ -0,0 +1,40 @@
+// array in js
+
+const myArr = [1,2,3,4]
+const myHeros = ["MadanSinghKeer", "BheruSinghKeer", "ShivSinghKeer", "OmSinghkeer"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr)
+// console.log(myArr[2])
+
+// array methods
+
+const myArr3 = [1,2,3,4,5,6,7,8,9]
+// myArr3.push(3)
+// myArr3.push(4)
+// myArr3.pop()
+// myArr3.unshift(9)
+// myArr3.shift(9)
+
+// console.log(myArr3.includes(91))
+// console.log(myArr3.indexOf(9))
+
+const newArray = myArr3.join()
+
+// console.log(typeof(newArray), newArray);
+// console.log(myArr3);
+
+// slice, splice
+
+// console.log("A", myArr3);
+
+const mynw = myArr3.slice(0,5)
+// console.log(mynw);
+
+const newars = [1, 2, 3, 4, 5]
+console.log(newars);
+const newarsx = newars.splice(1,3)
+console.log(newarsx);
+console.log(newars);
+
+
