ac30432700b87ba9ec3ceb4f0e7774c17dbb86a2
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-04 05:47:23 +0000

    objects

---
+++ 08_Objects/objects.js
@@ -0,0 +1,12 @@
+// Objects in js
+
+const js_User = {
+    name: "ShivSingh",
+    age: 19,
+    location: "Jaipur",
+    email: "shivam-frogiez@gmail.com",
+    isLoggedin: true
+}
+
+console.log(js_User.name);
+
