b8dd129de2c9db6f454acddfa83058a57286560c
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-08 05:33:55 +0000

    test

---
+++ 09_Practice/String_method.js
@@ -0,0 +1,29 @@
+// Q.1
+let animal = "Elephant"
+// console.log(animal[3])
+
+// Q.2
+let codex = "Javascript"
+// console.log(codex.slice(0,5))
+
+// Q.3
+let lowercasex = " Hello World "
+lowercasex = lowercasex.trim() //used to remove extra space
+// console.log(lowercasex.toLowerCase())
+
+// Q.4
+let repeatx = "ha"
+// console.log(repeatx.repeat(count : 4) : "ha")
+
+// Q.5
+let findx = "I Love to code in JS"
+// console.log(findx.search("code"));
+
+// Q.6
+let wishx = "Good day!"
+console.log(wishx.replace(day, night));
+
+
+
+
+
