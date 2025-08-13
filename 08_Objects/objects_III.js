9208055342c365654266c20a8fac45fd89fd857f
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-13 05:14:22 +0000

    JSON & Obj

--- 08_Objects/objects_III.js
+++ 08_Objects/objects_III.js
@@ -8,5 +8,24 @@ const course = {
 
 // console.log(course.courseInstructer);
 
-const {courseInstructer: intx} = course
+const {courseInstructer: intx} = course //this is called object de-structuring
 console.log(intx);
+
+
+// json format
+// json format - keys are also enclosed inside quotaion mark
+// {
+//     "name": "sam",
+//     "class": "VIII",
+//     "subject": "PCM"
+// }
+
+// // json format may be an object as above or may be an array as below
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+// We can use api to call json file and can understand api json file with json formatter->styleNBeatuify->
