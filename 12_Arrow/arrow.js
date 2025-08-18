8a14b09db15c97108198c4506f651dea563b7b30
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-18 05:35:57 +0000

    arrow

---
+++ 12_Arrow/arrow.js
@@ -0,0 +1,15 @@
+// how to use this and current contest in js
+
+const user1 = {
+    username: "ShivSinghKeer",
+    website: "Frogiez.Inc",
+
+    welcomeMessage: function(){
+        console.log(`The Username is ${this.username} and belongs to this ${this.website}`)
+        // console.log(this)
+    }
+}
+
+user1.username = "Gopal"
+user1.welcomeMessage()
+console.log(typeof(user1))
