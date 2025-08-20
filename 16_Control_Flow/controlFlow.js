fb58575b0a4d1217510419dbf7d3065dc542ad64
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-20 05:43:12 +0000

    control Flow

---
+++ 16_Control_Flow/controlFlow.js
@@ -0,0 +1,46 @@
+// IF statement
+// const IsUserLoggedIn = false
+
+// if (IsUserLoggedIn){
+//     console.log("First program of if statement")
+// } else{
+// console.log("Code Excuted")
+// }
+
+// < , > , <= , >= , == , ===, && , || , != , !==
+
+// if ( 1 >= 2 && 2 == 2){
+//     console.log("Checking Alpha")
+// }
+// else{
+//     console.log("Else Executed")
+// }
+
+// const score = 250
+
+// if (score > 200){
+//     const power = "fly"
+//     console.log(`The user can ${power}`)
+// }
+// else {
+//     console.log("Better Luck Nxt Time")
+// }
+// console.log(`The user can ${power}`)
+
+// const balance = 500
+
+// if ( balance > 400 ) console.log("You have Good Balance");
+// if ( balance > 400 ) console.log("You have Good Balance"), console.log("Yaa very Good");
+
+// using if else if and else 
+
+const userBalance = 300
+    if ( userBalance > 100 && userBalance < 300){
+        console.log(`Your Balance is btwn 100 & 300 and that is ${userBalance}`)
+    } 
+    else if ( userBalance < 300 && userBalance > 600 ){
+        console.log(`Your Balance is btwn 300 & 600 and that is ${userBalance}`)
+    }
+    else{
+        console.log("Code Excuted");
+    }
