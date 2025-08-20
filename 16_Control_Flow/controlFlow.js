3a3b117962c59490c0e93699ecca6a75a7c55253
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-20 06:06:59 +0000

    ControlFLow

--- 16_Control_Flow/controlFlow.js
+++ 16_Control_Flow/controlFlow.js
@@ -34,13 +34,20 @@
 
 // using if else if and else 
 
-const userBalance = 300
-    if ( userBalance > 100 && userBalance < 300){
-        console.log(`Your Balance is btwn 100 & 300 and that is ${userBalance}`)
-    } 
-    else if ( userBalance < 300 && userBalance > 600 ){
-        console.log(`Your Balance is btwn 300 & 600 and that is ${userBalance}`)
-    }
-    else{
-        console.log("Code Excuted");
-    }
+// const userBalance = 610
+//     if ( userBalance >= 100 && userBalance <= 300){
+//         console.log(`Your Balance is btwn 100 & 300 and that is ${userBalance}`)
+//     } 
+//     else if ( userBalance > 300 && userBalance <= 600 ){
+//         console.log(`Your Balance is btwn 300 & 600 and that is ${userBalance}`)
+//     }
+//     else{
+//         console.log("Code Excuted");
+//     }
+
+const IsUserLoggedIn = true
+const IsDebtCard = false
+
+if ( IsDebtCard && IsUserLoggedIn && true){
+    console.log("You can Log in Now")
+}
