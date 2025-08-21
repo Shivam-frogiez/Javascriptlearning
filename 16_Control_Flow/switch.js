7b1d82696290b144ad446547a65bb7158f1775fb
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-21 05:22:43 +0000

    switch statment

---
+++ 16_Control_Flow/switch.js
@@ -0,0 +1,15 @@
+//switch statement in js
+
+const balance = "03"
+
+switch (balance){
+    case "01":
+        console.log("January")
+        break;
+    case "02":
+        console.log("Febrary")
+        break;
+    case "03":
+        console.log("March")
+        break;
+}
