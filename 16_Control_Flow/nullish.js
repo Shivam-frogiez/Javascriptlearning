1afe0c076e1abd1a3725ca6642b0d010a95b50af
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-21 05:43:10 +0000

    nullish

---
+++ 16_Control_Flow/nullish.js
@@ -0,0 +1,9 @@
+// Nullish Coalescing Operator (??): null undefined
+
+let var1;
+// var1 = 10 ?? 39
+// var1 = null ?? 344
+// var1 = undefined ?? 242342
+var1 = null ?? 10 ?? 15
+
+console.log(var1)
