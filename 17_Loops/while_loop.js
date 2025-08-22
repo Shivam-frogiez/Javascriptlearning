152b8b6d43debd020da71e9fa05520dd8fdcdadb
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-22 05:58:14 +0000

    While Loop

---
+++ 17_Loops/while_loop.js
@@ -0,0 +1,19 @@
+// while loop in js
+
+let index = 0
+while (index <= 20) {
+    // console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+// while loop with array
+
+let myArray = [ "flash", "Batman", "Superman"]
+
+let arr = 0
+while (arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+// now do while loop video at 6:32:07 Video Node. 28
