fdff2b6806d7b4710e77f4c0a5e481a344679941
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-23 05:22:36 +0000

    for of loop

---
+++ 17_Loops/for_loop.js
@@ -0,0 +1,20 @@
+//for loop in js
+
+// ["", "", "", ""] this is string inside array
+// [{}, {}, {}, {}] this is object inside array
+
+// for of loop with array
+
+const myArr = [2, 4, 6, 8, 10]
+
+for (const i of myArr) {
+    // console.log(`The number is ${i}`)
+}
+
+// for of loop with string
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    console.log(greet);
+}
