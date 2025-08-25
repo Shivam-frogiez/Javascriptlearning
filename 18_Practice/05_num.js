9fe3d687c7d1ee66442e372212f7abab832c5281
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-25 06:52:07 +0000

    playground

--- 18_Practice/05_num.js
+++ 18_Practice/05_num.js
@@ -15,3 +15,55 @@ console.log(Math.floor(4.2))
 const myArr = [1234, 31, 12, 235, 23, 3412]
 
 // ????????????????????????????????????????????????????????????????
+
+
+
+
+// function rockPaperScissors() {
+//     const choices = ["rock", "paper", "scissors"];
+//     rl.question("Choose rock, paper, or scissors: ", (user) => {
+//         const computer = choices[Math.floor(Math.random() * 3)];
+//         console.log(`Computer chose: ${computer}`);
+//         if (user === computer) {
+//             console.log("It's a tie!");
+//         } else if (
+//             (user === "rock" && computer === "scissors") ||
+//             (user === "paper" && computer === "rock") ||
+//             (user === "scissors" && computer === "paper")
+//         ) {
+//             console.log("You win! 🎉");
+//         } else {
+//             console.log("You lose! 😢");
+// const readline = require("readline");
+
+// // Create input interface
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+//         }
+//     });
+// }
+// rockPaperScissors()
+
+
+const readline = require("readline");
+
+// Create input interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function passwordGenerator() {
+    rl.question("Enter password length: ", (len) => {
+        len = parseInt(len);
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        let password = "";
+        for (let i = 0; i < len; i++) {
+            password += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        console.log(`Generated Password: ${password}`);
+    });
+}
+passwordGenerator()
