8d6d8e8c4587c10fc7c9e0363119acda02402205
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-08-29 06:13:55 +0000

    Project

--- 19_ExpenseTrackerProject/self.js
+++ 19_ExpenseTrackerProject/self.js
@@ -3,28 +3,21 @@ const fs = require("fs") //here we are importing node.js build-in module to take
 
 const FILE_PATH = "19_ExpenseTrackerProject/selftry.json"; //assigning the path of the file
 
-//Below load data files from json file
 
-let expenses = (() => {
-    if (fs.existsSync(FILE_PATH)) {
-        const data = fs.readFileSync(FILE_PATH, "utf8");
-        return JSON.parse(data, (key, value) => {
-            if (key === "date") return new Date(value);
-            return value;
+
+//Below load data files from json file
+let expenses = (() => { //in expeses var if json had data then data is loaded in utf8 format i.e. text string else empty string is returened
+    if (fs.existsSync(FILE_PATH)) {  //existSync is a fs function - if file exist reutrn true else false from the FILE_PATH
+        const data = fs.readFileSync(FILE_PATH, "utf8"); //readFileSync is a fs function - read the JSON FILE in utf8 format
+        return JSON.parse(data, (key, value) => { //converts JSON String back to JavaScript object/Array
+            if (key === "date") return new Date(value); //if key is equal to date then it converts string into a Date object 
+            return value; //if key date not found then returns string value
         });
     }
-    return [];
+    return []; //if JSON file is empty reutns empty array
 })();
 
 
-
-// let expenses = (() => {
-//     if (fs.existsSync(FILE_PATH)) {
-//         const data = fs.readFileSync(FILE_PATH, "utf8");
-//         return JSON.parse(data, (key, value) => {
-//             if (key === "date") return new Date(value);
-//             return value;
-//         });
-//     } 
-//       return []; // ✅ Default empty array 
-// })();
+function saveExpenses() {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(expenses, null, 2));
+}
