2c1078f48d29593c1b936ae589e86cfc1d0e1ba2
Shivam-frogiez <shivam.frogiez@gmail.com>
2025-09-09 05:12:28 +0000

    practice

--- 20_Practice/math.js
+++ 20_Practice/math.js
@@ -3,4 +3,8 @@
 // console.log(Math.round(564654.5))
 // console.log(Math.floor(450.9))
 // console.log(Math.ceil(1239.1))
-console.log(Math.sqrt(16))
+// console.log(Math.sqrt(16))
+// console.log(Math.min(45,324235,234534,351,1,))
+// console.log(Math.max(45,324235,234534,351,1,324235.1))
+
+// console.log(Math.round(Math.random()*6));
