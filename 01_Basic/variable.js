bb14a86453c8f51f9a0a99473127c2347725d607
Shivam-frogiez <shivam.frogiez@gmail.com>
2026-01-08 05:52:15 +0000

    sdcfZ

--- 01_Basic/variable.js
+++ 01_Basic/variable.js
@@ -1,13 +1,19 @@
-const accountId = 14453
-let accountEmail = "Shiv@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
+// const accountId = 14453
+// let accountEmail = "Shiv@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
 
-// accountId = 2
+// // accountId = 2
 
-accountEmail = "This@gmail.com"
-accountPassword = "akf;lk"
-accountCity = "jaipur"
+// accountEmail = "This@gmail.com"
+// accountPassword = "akf;lk"
+// accountCity = "jaipur"
 
+// console.log(accountId)
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+const accountId = 56456
+let accountEmail = "shivsinghkeer"
+accountCity = "Nagda"
 console.log(accountId)
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountCity])
--- 04_String/string-methods.js
+++ 04_String/string-methods.js
@@ -1,28 +1,36 @@
-const gamerName = "Thisisexample"
-console.log(gamerName[0]);
-console.log(gamerName);
+// const gamerName = "Thisisexample"
+// // console.log(gamerName[0]);
+// // console.log(gamerName);
 
-// Q.1
-let animal = "Elephant"
-// console.log(animal[3])
+// // Q.1
+// let animal = "Elephant"
+// // console.log(animal[3])
 
-// Q.2
-let codex = "Javascript"
+// // Q.2
+// let codex = "Javascript"
 // console.log(codex.slice(0,5))
 
-// Q.3
-let lowercasex = " Hello World "
-lowercasex = lowercasex.trim() //used to remove extra space
-// console.log(lowercasex.toLowerCase())
+// // Q.3
+// let lowercasex = " Hello World "
+// lowercasex = lowercasex.trim() //used to remove extra space
+// // console.log(lowercasex.toLowerCase())
 
-// Q.4
-let repeatx = "ha"
-// console.log(repeatx.repeat(count : 4) : "ha")
+// // Q.4
+// let repeatx = "ha"
+// // console.log(repeatx.repeat(count : 4) : "ha")
 
-// Q.5
-let findx = "I Love to code in JS"
-// console.log(findx.search("code"));
+// // Q.5
+// let findx = "I Love to code in JS"
+// // console.log(findx.search("code"));
 
-// Q.6
-let wishx = "Good day!"
-console.log(wishx.replace(day, night));
+// // Q.6
+// let wishx = "Good day!"
+// // console.log(wishx.replace(day, night));
+
+let animal = "Lion is a king of Jungle!";
+// console.log(animal[1])
+// console.log(animal.slice(1,-1))
+// console.log(animal.toUpperCase())
+// console.log(animal.repeat(count : 4) : "Lion")
+// console.log(animal.search("a"))
+console.log(animal.replace("king", "lord"));
