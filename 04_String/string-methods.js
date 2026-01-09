e970c5f7891e34794f4808f885fef2dfb064bbf8
Shivam-frogiez <shivam.frogiez@gmail.com>
2026-01-09 05:23:25 +0000

    practice

--- 04_String/string-methods.js
+++ 04_String/string-methods.js
@@ -2,35 +2,67 @@
 // // console.log(gamerName[0]);
 // // console.log(gamerName);
 
+// const gamerName = "thisisforsample"
+// console.log(gamerName[10])
+
 // // Q.1
 // let animal = "Elephant"
 // // console.log(animal[3])
+// let animal = "Elephant"
+// console.log(animal[2])
 
 // // Q.2
 // let codex = "Javascript"
 // console.log(codex.slice(0,5))
 
+// let alpha = "Javascript"
+// console.log(alpha.slice(,5))
+
+
 // // Q.3
 // let lowercasex = " Hello World "
 // lowercasex = lowercasex.trim() //used to remove extra space
 // // console.log(lowercasex.toLowerCase())
+// let lowercasex = "Hello World "
+// lowercasex = lowercasex.trim()
+// console.log(lowercasex.length)
+
 
 // // Q.4
 // let repeatx = "ha"
-// // console.log(repeatx.repeat(count : 4) : "ha")
+// console.log(repeatx.repeat(4));
+
+// let repeater = "Yes Bunny!"
+// console.log(repeater.repeat(5))
+
 
 // // Q.5
 // let findx = "I Love to code in JS"
 // // console.log(findx.search("code"));
 
+// let finder = "I want to be a expert in JS"
+// console.log(finder.search("to"))
+
 // // Q.6
 // let wishx = "Good day!"
-// // console.log(wishx.replace(day, night));
+// // console.log(wishx.replace("day", "night"));
 
-let animal = "Lion is a king of Jungle!";
+// let wishx = "Good Day!"
+// console.log(wishx.replace("Day", "Night"));
+// console.log(wishx.replace("Day!", "Morning, Afternoon, Evening, Night"));
+
+// let animal = "Lion is a king of Jungle!";
 // console.log(animal[1])
 // console.log(animal.slice(1,-1))
 // console.log(animal.toUpperCase())
 // console.log(animal.repeat(count : 4) : "Lion")
 // console.log(animal.search("a"))
-console.log(animal.replace("king", "lord"));
+// console.log(animal.replace("king", "lord"));
+
+let tester = "This is the Tester apply operations   in it!"
+// testerx = tester.trim();
+// console.log(testerx.length)
+// tester = tester.repeat("is", 4)
+// console.log(tester)
+
+//Applicable opertaions :- search, slice, replace, repeat, length, trim, []
